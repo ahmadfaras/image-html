e3ac2f4264e17bbad1cc6c33181d3b1199207e81
Ahmad Zubair Faras <zubairfaras1@gmail.com>
2018-04-21 11:27:16 -0700

    server is serving html page

---
+++ server.js
@@ -0,0 +1,11 @@
+var express= require('express');
+var app= express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/app/'));
+
+
+app.listen(port, function(){
+	console.log('Server Started running on port http://localhost:' + port)
+})
+
